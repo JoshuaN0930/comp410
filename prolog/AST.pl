19cdad162ae7f8c778191a34f67827d993c02030
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2025-10-29 18:01:26 -0700

    hw assigments 3,4

--- prolog/AST.pl
+++ prolog/AST.pl
@@ -3,3 +3,15 @@ eval(add(E1,E2), Result) :-
     eval(E1, E1Result),
     eval(E2, E2Result),
     Result is E1Result + E2Result.
+
+eval(minus(E1,E2), Result) :-
+    eval(E1, E1Result),
+    eval(E2, E2Result),
+    Result is E1Result - E2Result.
+
+eval(mult(E1,E2), Result) :-
+    eval(E1, E1Result),
+    eval(E2, E2Result),
+    Result is E1Result * E2Result.
+
+eval
